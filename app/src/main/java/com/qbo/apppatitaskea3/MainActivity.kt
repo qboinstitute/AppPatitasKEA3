23b3c891d635ac80432dcb957e8c56287ad73254
LuisAngel <luisangel.salvat@gmail.com>
2021-02-18 20:13:16 -0500

    primera parte (180221)

--- app/src/main/java/com/qbo/apppatitaskea3/MainActivity.kt
+++ app/src/main/java/com/qbo/apppatitaskea3/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.qbo.apppatitaskea3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(Intent(this,
+                LoginActivity::class.java))
+        }, 3000)
     }
 }
