125b50be75caae076f2779b93d1191d8dfab67f8
LuisAngel <luisangel.salvat@gmail.com>
2021-02-25 19:47:42 -0500

    parte FINAL (250221)

--- app/src/main/java/com/qbo/apppatitaskea3/ui/VoluntarioFragment.kt
+++ app/src/main/java/com/qbo/apppatitaskea3/ui/VoluntarioFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.material.snackbar.Snackbar
 import com.qbo.apppatitaskea3.R
 import com.qbo.apppatitaskea3.databinding.FragmentVoluntarioBinding
+import com.qbo.apppatitaskea3.db.entity.PersonaEntity
+import com.qbo.apppatitaskea3.viewmodel.PersonaViewModel
 import org.json.JSONObject
 
 class VoluntarioFragment : Fragment() {
@@ -17,6 +23,8 @@ class VoluntarioFragment : Fragment() {
     private var _binding: FragmentVoluntarioBinding? = null
     private val binding get() = _binding!!
     private lateinit var colapeticiones: RequestQueue
+    private lateinit var personaEntity: PersonaEntity
+    private lateinit var personaViewModel: PersonaViewModel
 
 
     override fun onCreateView(
@@ -26,6 +34,18 @@ class VoluntarioFragment : Fragment() {
         _binding = FragmentVoluntarioBinding.inflate(inflater, container, false)
         val vista = binding.root
         colapeticiones = Volley.newRequestQueue(context)
+        personaViewModel = ViewModelProvider(this).get(PersonaViewModel::class.java)
+        personaViewModel.obtener()
+                .observe(viewLifecycleOwner, Observer {
+                    persona->
+                    persona?.let {
+                        if(persona.esvoluntario == "1"){
+                            actualizarFormulario()
+                        }else{
+                            personaEntity = persona
+                        }
+                    }
+                })
         binding.btnregvoluntario.setOnClickListener {
             if(binding.chkaceptocondiciones.isChecked){
                 binding.btnregvoluntario.isEnabled = false
@@ -41,7 +61,33 @@ class VoluntarioFragment : Fragment() {
     private fun registrarVoluntario(vista: View) {
         val urlvoluntario = "http://www.kreapps.biz/patitas/personavoluntaria.php"
         val parametroJson = JSONObject()
-        parametroJson.put("idpersona",1)
+        parametroJson.put("idpersona",personaEntity.id)
+        val request = JsonObjectRequest(
+                Request.Method.POST,
+                urlvoluntario,
+                parametroJson,
+                {
+                 response->
+                 if(response.getBoolean("rpta")){
+                     val nuevaPersonaEntity = PersonaEntity(
+                             personaEntity.id,
+                             personaEntity.nombres,
+                             personaEntity.apellidos,
+                             personaEntity.email,
+                             personaEntity.celular,
+                             personaEntity.usuario,
+                             personaEntity.password,
+                             "1"
+                     )
+                     personaViewModel.actualizar(nuevaPersonaEntity)
+                     actualizarFormulario()
+                 }
+                    mostrarMensaje(vista, response.getString("mensaje"))
+                    binding.btnregvoluntario.isEnabled = true
+                },{
+            binding.btnregvoluntario.isEnabled = true
+        })
+        colapeticiones.add(request)
     }
     fun actualizarFormulario(){
         binding.tvinfovoluntario.visibility = View.GONE
